e03e2e83edce65d54eb0f0ac1a1f0b45b508949d
Chris Rodriguez <chrismail627@gmail.com>
2022-12-24 13:46:44 -0800

    Added color toggles

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    toggleSiteColor('#a3c9ff');
+  });
 
 // The scroll listener.
-var $w = $(window).scroll(function(){
+var $w = $(window).scroll(function () {
     var scrollVal = $w.scrollTop();
 
     toggleClass("#scroll-label", "no-display", scrollVal, 30);
@@ -12,9 +15,28 @@ var $w = $(window).scroll(function(){
 
 // Mimics ngClass from AngularJS. Adds a class if a threshold is reached; else, remove the class.
 function toggleClass(attribute, clss, scrollVal, threshold) {
-    if ( scrollVal > threshold ) {   
+    if (scrollVal > threshold) {
         $(attribute).addClass(clss);
     } else {
         $(attribute).removeClass(clss);
     }
+}
+
+function toggleSiteColor(color) {
+    // Update the color responsive elements
+    var colorResponsiveElements = document.getElementsByClassName('color-toggleable');
+    for (const element of colorResponsiveElements) {
+        element.style.backgroundColor = color;
+        console.log("item");
+    }
+
+    // Remove any enabled toggles
+    var colorToggles = document.getElementsByClassName('color-toggle');
+    for (const element of colorToggles) {
+        element.classList.remove('color-toggle-enabled');
+    }
+
+    // Enable the toggle that should be enabled
+    var selectedElement = document.getElementById(color);
+    selectedElement.classList.add('color-toggle-enabled');
 }
