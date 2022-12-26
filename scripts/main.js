c00af4300848c8e43e5f70488e60c3c7233d91df
Chris Rodriguez <chrismail627@gmail.com>
2022-12-26 09:33:42 -0800

    Some cleanup of the hello world label

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ var $w = $(window).scroll(function () {
     var scrollVal = $w.scrollTop();
 
     toggleClass("#scroll-label", "no-display", scrollVal, 50);
-    toggleClass("#hello-world-label", "no-show", scrollVal, 50);
+    toggleClass("#hello-world-container", "no-show", scrollVal, 50);
     toggleClass("#left-feature-div", "full-size", scrollVal, 50);
     toggleClass("#top-content", "visible", scrollVal, 50);
     toggleClass("#top-bio-bar", "bar-larger", scrollVal, 55);
